603de39e1e054d2c8370944c4f7e74a0d5f88470
Andrew Khachatryan <hacatranandrej393@gmail.com>
2026-04-28 17:52:33 +0300

    Выполнено ДЗ по теории вероятности

---
+++ homework4.js
@@ -0,0 +1,25 @@
+function checkProbabilityTheory(count) {
+    let evenCount = 0;
+    let oddCount = 0;
+
+    for (let i = 0; i < count; i++) {
+    
+        let randomNumber = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+
+        if (randomNumber % 2 === 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+    }
+
+    let evenPercent = (evenCount / count) * 100;
+    let oddPercent = (oddCount / count) * 100;
+
+    console.log("Кількість згенерованих чисел: " + count);
+    console.log("Парних чисел: " + evenCount);
+    console.log("Не парних чисел: " + oddCount);
+    console.log("Відсоток парних до не парних: " + evenPercent.toFixed(2) + "% / " + oddPercent.toFixed(2) + "%");
+}
+
+checkProbabilityTheory(1000);
